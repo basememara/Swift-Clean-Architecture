82e1fbb906e52f7a80cf31bf899a561a9ff61739
Basem Emara <contact@basememara.com>
2018-04-18 10:06:10 -0400

    Initial Commit

---
+++ Swift Clean Architecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Clean Architecture
+//
+//  Created by Basem Emara on 2018-04-18.
+//  Copyright © 2018 Basem Emara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
